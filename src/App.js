ae33131ececf409a44a83b8fb38f250ac20b1077
amit9554 <tamit1316@gmail.com>
2024-10-09 10:30:14 +0530

    update Creating project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import SketchBoard from './components/SketchBoard';
+
+function App() {
+  return (
+    <div className="App">
+      <SketchBoard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SketchBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SketchBoard = () => {
+  return (
+    <div>SketchBoard</div>
+  )
+}
+
+export default SketchBoard
