8cba9f78afd5dbd2a271885909efc3578e13a66d
amit9554 <tamit1316@gmail.com>
2024-10-11 12:04:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import SketchBoard from './components/SketchBoard';
+import ColorBoard from './components/ColorBoard'
 
 function App() {
   return (
     <div className="App">
       <SketchBoard />
-    
+      {/* <ColorBoard /> */}
+
     </div>
   );
 }
